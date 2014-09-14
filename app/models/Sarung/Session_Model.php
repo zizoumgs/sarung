08ece7d0d710aa8a79a208dd1522277a96ce8d7b
Sakip Alias ZizouMgs <zizoumgs@hotmail.com>
2014-09-15 06:37:41 +0700

    Add Kalender Crud

--- app/models/Sarung/Session_Model.php
+++ app/models/Sarung/Session_Model.php
@@ -1,6 +1,14 @@
 <?php
+/**	
+	Many to Many
+	Three database tables are needed for this relationship: users, roles, and role_user.
+	The role_user table is derived from the alphabetical order of the related model names,
+	and should have user_id and role_id columns.
+*/
 //class Session extends Sarung_Model_Root{
 class Session_Model extends Sarung_Model_Root{
     protected $table = 'session';
-	//protected $connection = 'fusarung';
+    public function kalender(){
+       return $this->belongsToMany('Session_Model' ,'Kalender' , 'idsession' , 'idevent');
+    }
 }

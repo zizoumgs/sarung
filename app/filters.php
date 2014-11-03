01ea73a4e0c675c3ef2d23dccfe24c3b85fc5b7f
Sakip Alias ZizouMgs <zizoumgs@hotmail.com>
2014-11-03 13:08:11 +0700

    Filter for mismatch token

--- app/filters.php
+++ app/filters.php
@@ -86,7 +86,8 @@ Route::filter('csrf', function()
 	$token = Request::ajax() ? Request::header('X-CSRF-Token') : Input::get('_token');
 	if (Session::token() != $token)
 	{
-		throw new Illuminate\Session\TokenMismatchException;
+		//throw new Illuminate\Session\TokenMismatchException;
+		return Redirect::to('/');
 	}
 });
 /**
